59d873bc4e6f5b26c8ced0c17bcebbd4110c4c3c
Angel Sanchez <angelsanmag@gmail.com>
2019-04-04 20:10:34 +0100

    AddToScreen adds verticaly

--- calcuscript.js
+++ calcuscript.js
@@ -1,4 +1,5 @@
 const array = [1,2];
+let opt
 
 const sum = (accumulator, currentValue) => accumulator + currentValue;
 const subtract = (accumulator, currentValue) => accumulator - currentValue;
@@ -9,3 +10,15 @@ console.log(array.reduce(sum));
 console.log(array.reduce(subtract));
 console.log(array.reduce(multiply));
 console.log(array.reduce(divide));
+
+function addToScreen(opt) {
+let screenBox = document.querySelector('#screen')
+let numeroScr = document.createElement('p');
+numeroScr.textContent = opt
+screenBox.appendChild(numeroScr);
+}
+
+let uno = document.querySelector('#one');
+uno.addEventListener('click', () => {
+    addToScreen([1]);
+})
