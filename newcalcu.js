adce130ebbb0aaed882a544e1b9cdd62c7e4fbd1
Angel Magalhães <angel.magalhaes@etma.local>
2019-04-09 14:29:56 +0100

    add number query selector

--- newcalcu.js
+++ newcalcu.js
@@ -1,7 +1,9 @@
-let a = 1
-let b = 2
+let a
+let b
 let op
 let result
+let visor = document.querySelector("#screen")
+let operArray = [];
 
 function operate(op) {
     switch (op) {
@@ -18,5 +20,20 @@ function operate(op) {
         result = a/b;
         break;
     }
-    console.log(result)
+    console.log(result);
 }
+
+function addToScreen(numeral) {
+    let numVisor = document.createElement('div');
+    numVisor.classList.add("numScr")
+    numVisor.textContent = numeral;
+    visor.appendChild(numVisor);
+}
+
+// NUMBER QUERY SELECTORS
+let uno = document.querySelector('#one');
+uno.addEventListener('click', () => {
+    if (operArray.length < 18) { 
+    addToScreen(1)
+    operArray.push(1)}
+});
