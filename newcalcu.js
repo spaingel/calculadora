f24f8dab348b11b7428afc11117287cec169f3b3
Angel Magalhães <angel.magalhaes@etma.local>
2019-04-09 14:34:12 +0100

    All Numbers added

--- newcalcu.js
+++ newcalcu.js
@@ -37,3 +37,65 @@ uno.addEventListener('click', () => {
     addToScreen(1)
     operArray.push(1)}
 });
+
+let dos = document.querySelector('#two');
+dos.addEventListener('click', () => {
+    if (operArray.length < 18) { 
+    addToScreen(2)
+    operArray.push(2)}
+});
+let tres = document.querySelector('#three');
+tres.addEventListener('click', () => {
+    if (operArray.length < 18) { 
+        addToScreen(3)
+        operArray.push(3)}
+});
+
+let cuatro = document.querySelector('#four');
+cuatro.addEventListener('click', () => {
+    if (operArray.length < 18) { 
+        addToScreen(4)
+        operArray.push(4)}
+});
+
+let cinco = document.querySelector('#five');
+cinco.addEventListener('click', () => {
+    if (operArray.length < 18) { 
+        addToScreen(5)
+        operArray.push(5)}
+});
+
+let seis = document.querySelector('#six');
+seis.addEventListener('click', () => {
+    if (operArray.length < 18) { 
+        addToScreen(6)
+        operArray.push(6)}
+});
+
+let siete = document.querySelector('#seven');
+siete.addEventListener('click', () => {
+    if (operArray.length < 18) { 
+        addToScreen(7)
+        operArray.push(7)}
+});
+
+let ocho = document.querySelector('#eight');
+ocho.addEventListener('click', () => {
+    if (operArray.length < 18) { 
+        addToScreen(8)
+        operArray.push(8)}
+});
+
+let nueve = document.querySelector('#nine');
+nueve.addEventListener('click', () => {
+    if (operArray.length < 18) { 
+        addToScreen(9)
+        operArray.push(9)}
+});
+
+let cero = document.querySelector('#zero');
+cero.addEventListener('click', () => {
+    if (operArray.length < 18) { 
+        addToScreen(0)
+        operArray.push(0)}
+});
